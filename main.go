545c2fae1d503d3fdbffd3d2769c88bcf127cb56
Risuban <nicotapiamunoz@gmail.com>
2023-04-15 18:20:22 -0400

    changed ip to 127.0.0.1

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	router.POST("/albums", postAlbums)
 	router.GET("/albums/:id", getAlbumByID)
 
-	router.Run("localhost:8080")
+	router.Run("127.0.0.1:8080")
 }
 
 // getAlbums responds with the list of all albums as JSON.
