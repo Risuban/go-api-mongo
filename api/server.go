5f7afca675a05a5a998ecfaec8f1fdb7c6265e03
ma_paz <maria.poblete@sansano.usm.cl>
2023-04-16 17:29:01 -0400

    uw

--- api/server.go
+++ api/server.go
@@ -1,17 +1,9 @@
 package api
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
 type avion struct {
 	Modelo             string `json:"modelo"`
 	Numero_de_serie    string `json:"numero_de_serie"`
@@ -58,44 +50,18 @@ type destino struct {
 	Pasajeros []pasajero `json:"pasajeros"`
 }
 
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 500},
-}
-
 // getAlbums responds with the list of all albums as JSON.
 func GetAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+
 }
 
 // postAlbums adds an album from JSON received in the request body.
 func PostAlbums(c *gin.Context) {
-	var newAlbum album
 
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
 // getAlbumByID locates the album whose ID value matches the id
 // parameter sent by the client, then returns that album as a response.
 func GetAlbumByID(c *gin.Context) {
-	id := c.Param("id")
 
-	// Loop over the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
--- main.go
+++ main.go
@@ -5,29 +5,13 @@ import (
 	a "mongoapi/api"
 	"os"
 
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 
 	"github.com/joho/godotenv"
 )
-
-// estoy siguiendo el turo, aqui va la declaración de structs
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-// Rellenar para poblar vars
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 500},
-}
 
 // definir bien puerto
 func main() {
@@ -42,11 +26,14 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(client)
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
 	router := gin.Default()
 	router.GET("/albums", a.GetAlbums)
 	router.POST("/albums", a.PostAlbums)
 	router.GET("/albums/:id", a.GetAlbumByID)
 	addr := server + ":" + port
 	router.Run(addr)
+
 }
